df82f5c39a127e268850f73f81228029c197c9ac
Patryk Konarzewski <p.konarzewski@supergrosz.pl>
2019-07-24 15:50:21 +0200

    formating

--- wtlogger/utils.py
+++ wtlogger/utils.py
@@ -7,14 +7,14 @@ from typing import Union
 import wtlogger.config as conf
 
 
-def system_shutdown(freeze:int=10) -> None:
+def system_shutdown(freeze: int = 10) -> None:
     """Shutdown system."""
 
     sys_name = platform.system()
 
-    conf.LOGGER.info('Shutting down system.')
+    conf.LOGGER.info("Shutting down system.")
 
-    if sys_name.upper() == 'WINDOWS':
+    if sys_name.upper() == "WINDOWS":
         run("shutdown /s /t {}".format(freeze), shell=True)
     else:
         conf.LOGGER.exception('Operating system "{}" not supported'.format(sys_name))
